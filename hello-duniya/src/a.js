b471a4b79eec3bd63d682e435fb2e1c648d9a3a0
pranavbadhan8 <pranavbadhan@example.com>
2023-07-11 11:58:46 +0530

    submitting code-Tue Jul 11 2023 11:58:45 GMT+0530 (India Standard Time)

---
+++ hello-duniya/src/a.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import c from './b.js';
+
+console.log(c);
---
+++ hello-duniya/src/b.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { ReactDOM } from "react";
+
+export let a = 20;
+
+export let b = 40;
+
