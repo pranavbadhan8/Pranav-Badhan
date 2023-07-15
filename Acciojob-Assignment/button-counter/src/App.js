380f40f8c19de72994dc3b6a75dd1085ece94f53
pranavbadhan8 <pranavbadhan@example.com>
2023-07-15 22:54:49 +0530

    submitting code-Sat Jul 15 2023 22:54:46 GMT+0530 (India Standard Time)

--- Acciojob-Assignment/button-counter/src/App.js
+++ Acciojob-Assignment/button-counter/src/App.js
@@ -7,12 +7,12 @@ function App() {
    let[count,setCount] = useState(0); 
   
     function change(){
-       setCount("Button clicked"+count+1 +"times")
+       setCount(count+1)
     }
 
   return (
     <div className="App">
-      <p>{count}</p>
+      <p>Button clicked {count} times</p>
       <button onClick={change}>click me</button>
     </div>
   );
